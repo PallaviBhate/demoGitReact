1ac2bccbe9ad3d5ac7b4e34c5585009b1a987dfd
Pallavi Bhate <PB00476956@TechMahindra.com>
2020-07-17 15:11:10 +0530

    Updates

---
+++ precache-manifest.4a1722ee2f933613ba63b21f5521b8b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b741cfc6706522aaac1ba6fb5ed3de",
+    "url": "/demoGitReact/index.html"
+  },
+  {
+    "revision": "ce7e0ee297b6196f7f55",
+    "url": "/demoGitReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "38c319ab82a2dc6ba2a4",
+    "url": "/demoGitReact/static/js/2.2d274547.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demoGitReact/static/js/2.2d274547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce7e0ee297b6196f7f55",
+    "url": "/demoGitReact/static/js/main.d950c193.chunk.js"
+  },
+  {
+    "revision": "ebf8d97b44644e6dddfe",
+    "url": "/demoGitReact/static/js/runtime-main.b611925e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demoGitReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
